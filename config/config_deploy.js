8181d9321751c0d940c0c87e811da24311f00d67
polina-tsvilodub <42241414+polina-tsvilodub@users.noreply.github.com>
2018-08-10 18:14:55 +0200

    added text input instead of binary choice

---
+++ config/config_deploy.js
@@ -0,0 +1,20 @@
+var config_deploy = {
+    
+    // obligatory fields
+	
+    // the experimentID is needed to recover data from the _babe server app
+	// you receive the experimentID when you create the experiment using the _babe server app
+	"experimentID": "8",
+	// if you use the _babe server app, specify its URL here
+	"serverAppURL" : "https://babe-backend.herokuapp.com/api/submit_experiment/",
+
+    // set deployment method; use one of:
+    //'debug', 'localServer', 'MTurk', 
+    // 'MTurkSandbox', 'Prolific', 'directLink'
+    "deployMethod" : "debug", 
+    
+    // optional fields
+    
+    // who to contact in case of trouble
+    "contact_email": "YOUREMAIL@wherelifeisgreat.you", 
+};
---
+++ trial_info/practice_trials.js
@@ -0,0 +1,6 @@
+var practice_trials = [
+	{question: "Сколько на картинке красных точек?",
+	 picture: "images/question_mark_03.jpg"},
+	{question: "Сколько на картинке красных точек?",
+	 picture: "images/question_mark_04.png"},
+];
