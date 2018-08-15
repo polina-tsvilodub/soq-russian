1412dbbd42036243d5ab46bda862aa27b17b427b
Michael Franke <the.secret.goldfish@me.com>
2018-08-15 09:31:26 +0300

    set up pilot live experiment

--- config/config_deploy.js
+++ config/config_deploy.js
@@ -4,9 +4,9 @@ var config_deploy = {
 	
     // the experimentID is needed to recover data from the _babe server app
 	// you receive the experimentID when you create the experiment using the _babe server app
-	"experimentID": "2",
+	"experimentID": "3",
 	// if you use the _babe server app, specify its URL here
-	"serverAppURL" : "https://mcmpact.ikw.uni-osnabrueck.de/babe/api/submit_experiment",
+	"serverAppURL" : "https://mcmpact.ikw.uni-osnabrueck.de/babe/api/submit_experiment/",
 
     // set deployment method; use one of:
     //'debug', 'localServer', 'MTurk', 
@@ -16,5 +16,5 @@ var config_deploy = {
     // optional fields
     
     // who to contact in case of trouble
-    "contact_email": "mchfranke@gmail.com", 
+    "contact_email": "ptsvilodub@uni-osnabrueck.de", 
 };
